fbe4bf480cd0e7f522537ea0aa67fc29007d15cc
Ekolinnia <89620061+Ekolinnia@users.noreply.github.com>
2023-04-10 12:13:03 -0400

    no message

--- soen341/testing/StudentApplicationTest.php
+++ soen341/testing/StudentApplicationTest.php
@@ -6,47 +6,51 @@ use PHP\Framework\TestCase;
 
 class SignUpTest extends TestCase
 {
-    public function testSuccessApp() {
+    public function testSuccessApp()
+    {
         //student log-in
         $_SESSION['username'] = 'Anna';
 
         //connect to database 
-        $conn = new mysqli("localhost", "root", DB_PASSWORD,"application" );
+        $conn = new mysqli("localhost", "root", DB_PASSWORD, "application");
         //makes sure no other applications (clears)
-        $conn ->query("DELETE FROM applications");
+        $conn->query("DELETE FROM applications");
 
         //submitting an application
         $postingid = 321;
-        $form = array ('id', $postingid);
-        $applicationResponse = $this -> post('/BACK_apply.php', $form);
+        $form = array('id', $postingid);
+        $applicationResponse = $this->post('/BACK_apply.php', $form);
 
         //make sure that the response to the application leads to the succes page
-        $this ->assertEquals(302, $applicationResponse ->getStatusCode());
+        $this->assertEquals(302, $applicationResponse->getStatusCode());
         //retrieve header of location 
-        $this ->assertEquals( '/success_apply_position.php', $applicationResponse->getHeaderLine('Location'));
+        $this->assertEquals('/success_apply_position.php', $applicationResponse->getHeaderLine('Location'));
 
         //check if application is saved in database
         $result = $conn->query("SELECT * FROM applications WHERE postingid = $postingid AND student = 'Anna'");
-       
+
         $this->assertEquals(1, $result->num_rows);
 
         $this->clear();
-
-        //cleanup - clear at the end of testing
-
-        protected function clear(): void {
-            // Set up the database connection
-            $conn = new mysqli("localhost", "root", DB_PASSWORD, "applications");
-        
-            // Delete any rows that were inserted during the test
-            $conn->query("DELETE FROM applications");
-        
-            // Close the database connection
-            $conn->close();
-        }
-
-        
-
     }
+
+    //cleanup - clear at the end of testing
+
+
+    protected function clear(): void
+    {
+        // Set up the database connection
+        $conn = new mysqli("localhost", "root", DB_PASSWORD, "applications");
+
+        // Delete any rows that were inserted during the test
+        $conn->query("DELETE FROM applications");
+
+        // Close the database connection
+        $conn->close();
+    }
+
+
+
 }
+
 ?>
